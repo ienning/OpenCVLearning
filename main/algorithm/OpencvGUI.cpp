aff2d572d80c481507c5b96888b281bea4dd4313
ienning <enningwan@gmail.com>
2022-08-31 17:50:37 +0800

    delete project

--- main/algorithm/OpencvGUI.cpp
+++
@@ -1,6 +0,0 @@
-#include "OpencvGUI.h"
-
-OpenCVGUI::OpenCVGUI()
-{
-	std::cout << "hello GUI" << std::endl;
-}
--- main/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "algorithm/OpencvGUI.h"
-
-int main(int argc, char** argv)
-{
-	OpenCVGUI opencvGUI;
-	return 0;
-}
